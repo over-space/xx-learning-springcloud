6cd8b715fcce4c53da52903c19c53ae94bc42997
lifang <xxplus@outlook.com>
2020-09-01 10:23:39 +0800

    zuul 灰度发布

---
+++ springcloud-eureka-feign/src/main/java/com/learning/springcloud/feign/SpringcloudEurekaFeignApplication.java
@@ -0,0 +1,21 @@
+package com.learning.springcloud.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableDiscoveryClient
+@ComponentScan("com.learning.springcloud")
+public class SpringcloudEurekaFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudEurekaFeignApplication.class, args);
+	}
+
+}
