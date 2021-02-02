60f3351c7558d6ec4946e4260f3ffe0408eb6bbc
lifang <lifang@jingkunsystem.com>
2021-02-02 17:36:43 +0800

    activeMQ demo

---
+++ springcloud-mq/src/main/java/com/learning/mq/SpringcloudMqApplication.java
@@ -0,0 +1,13 @@
+package com.learning.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudMqApplication.class, args);
+    }
+
+}
---
+++ springcloud-mq/src/test/java/com/learning/mq/SpringcloudMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
